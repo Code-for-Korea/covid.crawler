d01b832430d201166eb57102f120b0825f0275cd
Keriel <keriel@jupiterflow.com>
2020-03-24 17:50:25 +0900

    release corona-stat-scraper-kor

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use('/api', require('./routes/api'));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server connected!');
+});
