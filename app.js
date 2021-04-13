e19c37beb3dd9989b11f6e28ffc0777e4c77a739
Seia-Soto <seia@outlook.kr>
2021-04-13 21:08:18 +0900

    chore: add ci and fix code style

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const app = express();
+const express = require('express')
 
-app.use('/api', require('./routes/api'));
+const app = express()
+
+app.use('/api', require('./routes/api'))
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('server connected!');
-});
+  console.log('server connected!')
+})
